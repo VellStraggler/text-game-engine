65eb6f81f029f8e4dd869ea92181761b69d678f9
David Wells <wel20012@byui.edu>
2021-10-08 03:06:48 -0600

    map is being stored, code is being read, and key listening is running side by side to map printing

--- code_translator.py
+++ code_translator.py
@@ -1,49 +1,123 @@
 import tkinter as tk
 from pynput import keyboard
+import time, threading
 
 def main():
     #get the name of the code to be used.
-    code_file_name = input('Enter "code.txt": ')
+    code_file_name = 'textengine/example_code.txt'
+    print(f'Using file path: {code_file_name}')
     #run this input loops until the file is usable
+    '''
     while not can_be_read(code_file_name):
         print('Error: Incorrect File Name')
         code_file_name = input('Enter "example_code.txt": ')
+    '''
     #read code line by line
     lines = store_code(code_file_name)
     
-    #translate code
-    translate(lines)
+    #compile code
+    map_array = compile(lines)
 
     #Start up key listener
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
-    listener.join()
 
     #begin the printing loop
     #For now, game will be running within a terminal
+    class printThread (threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            print('Thread two running...')
+        def run(self):
+            while(True):
+                map_array.print_all()
+                time.sleep(.5)
 
-def translate(lines):
-    #Store variables in a dictionary
-    for line in lines:
-        words = line.split(" ")
-        if words[1] == '=':
-            #FOR VARIABLE DECLARATIONS
-            if words[2].find("("):
-                #FOR CLASSES
-                #map, sprites, sprite
-                classtype = words[2][:words[2].find("(")]
-                if classtype == "sprite":
-                    #Most common occurrence probably
-                    print(classtype)
-                elif classtype == "map":
-                    print(classtype)
-                elif classtype == "sprites":
-                    print(classtype)
-                else: print(f'Error: Class type not recognized: Line {line}')
+    p_thread = printThread()
+    p_thread.start()
 
+class MapObject():
+    def __init__(self):
+        self.map_path = ''
+        self.map_array = []
 
+    def set_path(self,map_path):
+        self.map_path = map_path
+        self.store_map()
+        self.map_patcher()
+
+    def store_map(self):
+        #stores text from file as 2D array or list: [[x1,x2,x3],[x1,x2,x3]]
+        with open(self.map_path,'r') as file:
+            currentline = file.readline() #a string
+            while currentline:
+                xlist = []
+                #remove any newline calls
+                currentline = currentline[:-1]
+                for char in currentline:
+                    xlist.append(char)
+                self.map_array.append(xlist)
+                currentline = file.readline()
             
-        
+
+    def map_patcher(self):
+        #adds spaces until the array is rectangular
+        #find longest y:
+        longest_y = 0
+        #the length of map_array is the extent of y
+        for y in range(0,len(self.map_array)):
+            if len(self.map_array[y]) > longest_y:
+                longest_y = len(self.map_array[y])
+        #make all columns the same length
+        for row in self.map_array:
+            for s in range(0,longest_y - len(row)):
+                row.append(" ")
+
+    def print_all(self):
+        for yrow in self.map_array:
+            for yitem in yrow:
+                print(yitem,end="")
+            print()
+            
+
+def compile(lines):
+    #Store variables in a dictionary
+    map_array = MapObject()
+    print(len(lines))
+    for i in range(0,len(lines)):
+        words = lines[i].split()
+        if len(words) > 1:
+            if words[1] == '=':
+                #FOR VARIABLE DECLARATIONS
+                if words[2].find(")") == len(words[2])-1:
+                    #FOR CLASSES
+                    #map, sprites, sprite
+                    classtype = words[2][:words[2].find("(")]
+                    if classtype == "sprite":
+                        #Most common occurrence probably
+                        print(classtype)
+                    elif classtype == "map":
+                        #locate map file
+                        mapname = dollar_word(words[2])
+                        #create Map Object of file
+                        map_array.set_path(mapname)
+                        print(f'Map Found using path: {mapname}')
+                    elif classtype == "sprites":
+                        print(classtype)
+                    else: print(f'Error: Class type not recognized: Line {lines[i]}')
+                elif words[0] == 'window_resolution':
+                    print(f'Note: Resolution Found: {words[2]}')
+                else:
+                    print(f'No Assignment found on line {i}')
+            else: print(f'Error: All lines of code should include an "=" assignment')
+    return map_array
+
+
+#simple function to find words between $ signs
+def dollar_word(word):
+    word = word[(word.find('$') + 1):]
+    return word[:(word.find('$'))]
+
 #KEY LISTENING
 def on_press(key):
     #must be called AFTER keyboard is 
@@ -68,24 +142,13 @@ def store_code(code_file_name):
     #Empty lines and comment lines are skipped
     #read-only
     with open(code_file_name, 'r') as code:
-        currentline = code_file_name.readline()
-        if len(currentline) > 0:
-            if currentline[0] != '#':
-                lines.append(currentline)
+        currentline = code.readline()
+        while(currentline):
+            if len(currentline) > 0:
+                if currentline[0] != '#':
+                    lines.append(currentline)
+            currentline = code.readline()
     return lines
     #returns a list of strings
-
-def store_map(map_file_name):
-    #Store map in 2D Array
-    map_array = [[]]
-    with open(map_file_name,'r') as file:
-        currentline = file.readline()
-        y = 0
-        while currentline:
-            for x in range(0,len(currentline)):
-                map_array[y][x] = currentline[x]
-            y += 1
-            currentline = file.readline()
-    return map_array()
 
 main()
