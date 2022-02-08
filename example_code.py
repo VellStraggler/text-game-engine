578fa7e4fd752443a3003204a645602259224326
David Wells <wel20012@byui.edu>
2022-02-08 00:00:11 -0700

    More cleaning. draw_map won't convert

--- example_code.py
+++ example_code.py
@@ -4,17 +4,14 @@ g = tx.GameObject()
 g.map.path = 'example_map.txt'
 g.map.translate()
 g.sprites.path = 'spritesheet1.txt'
+g.sprites.get_sprites()
 points = 0
 timeofday = 4
 
-player = tx.Sprite('giant-head','p',[3,4])
+player = g.sprites.Sprite('giant-head','p',[3,4])
 player.movement = True
-g.sprites.add(player)
-tree = tx.Sprite('tree','t',[7,6])
-g.sprites.add(tree)
-cottage = tx.Sprite('cottage','c')
-g.sprites.add(cottage)
-brick = tx.Sprite('wall','w')
-g.sprites.add(brick)
+tree = g.sprites.Sprite('tree','t',[7,6])
+cottage = g.sprites.Sprite('cottage','c')
+brick = g.sprites.Sprite('wall','w')
 
 g.run_map()
