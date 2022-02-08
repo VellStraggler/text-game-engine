559aa0f3208de6eb52a766cd337726e469bfe83e
David Wells <wel20012@byui.edu>
2022-02-07 20:24:48 -0700

    Reorganizing Classes

--- example_code.py
+++ example_code.py
@@ -1,24 +1,14 @@
 import textengine as tx
 
-game = tx.GameObject()
-map = game.Map('example_map.txt')
-sprites = game.Sprites('spritesheet1.txt')
+g = tx.GameObject()
+g.map.path = 'example_map.txt'
+g.map.translate()
+g.sprites.path = 'spritesheet1.txt'
 points = 0
 timeofday = 4
 
-
-#Sprites
-player = Sprite('giant-head')
-player.on_map = 'p'
-player.xy = 3,4
+player = g.sprites.add('giant-head','p',[3,4])
 player.movement = True
-tree = sprite('tree')
-tree.on_map = 't'
-tree.xy = 7,6
-
-cottage = sprite('cottage')
-cottage.on_map = 'c'
-brick = sprite('wall')
-brick.on_map = 'w'
-
-
+tree = g.sprites.add('tree','t',[7,6])
+cottage = g.sprites.add('cottage','c')
+brick = g.sprites.add('wall','w')
